2cae2207600dd33702f2479872483a59c68cc022
Wyatt Lake <wattttte@gmail.com>
2021-02-06 22:10:18 -0800

    Fixed svg color issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import { Navbar } from "../components/Navbar";
 import { Button } from "../components/Button";
 import Head from "next/head";
 import Logo from "../public/svgs/logo.svg";
+import Icon1 from "../public/svgs/icon1.svg";
+import Icon2 from "../public/svgs/icon2.svg";
+import Icon3 from "../public/svgs/icon3.svg";
 
 export default function Home() {
     return (
@@ -28,7 +31,12 @@ export default function Home() {
                 <section className={styles.body}>
                     <div className={styles.glassBox}>
                         <div className={styles.shortParagraph}>
-                            <h2 className={styles.sectionHeader}>About me</h2>
+                            <div className={styles.sectionHeader}>
+                                <h2 className={styles.sectionHeaderText}>
+                                    About me
+                                </h2>
+                                <Icon1 className={styles.smallIcon} />
+                            </div>
                             <p className={styles.sectionText}>
                                 I’m a 14 year old programmer and designer from
                                 California. I mainly work with Rust and
@@ -36,9 +44,12 @@ export default function Home() {
                             </p>
                         </div>
                         <div className={styles.shortParagraph}>
-                            <h2 className={styles.sectionHeader}>
-                                Programming
-                            </h2>
+                            <div className={styles.sectionHeader}>
+                                <h2 className={styles.sectionHeaderText}>
+                                    Programming
+                                </h2>
+                                <Icon2 className={styles.smallIcon} />
+                            </div>
                             <p className={styles.sectionText}>
                                 Currently, I am working on a rasterizer written
                                 in Rust and a notes application. You can check
@@ -46,9 +57,12 @@ export default function Home() {
                             </p>
                         </div>
                         <div className={styles.shortParagraph}>
-                            <h2 className={styles.sectionHeader}>
-                                About this site
-                            </h2>
+                            <div className={styles.sectionHeader}>
+                                <h2 className={styles.sectionHeaderText}>
+                                    About this site
+                                </h2>
+                                <Icon3 className={styles.smallIcon} />
+                            </div>
                             <p className={styles.sectionText}>
                                 This site is a collection of all my best
                                 programming projects and designs. In the future,
